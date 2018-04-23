ee5e57170314cc98833feb21dd7cf52812e0f120
Matias Valdes <matvaldesg@gmail.com>
2018-04-23 11:59:13 -0400

    Se implmenta Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,11 +9,18 @@ import { CommonModule } from '@angular/common';
 })
 export class AppRoutingModule { }
 */
+
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HeroesComponent }      from './heroes/heroes.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent }
 ];
 
@@ -21,4 +28,5 @@ const routes: Routes = [
   exports: [ RouterModule ],
   imports: [ RouterModule.forRoot(routes) ]
 })
+
 export class AppRoutingModule {}
